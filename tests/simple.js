49111d37f76f4077b5d94bf761a91c973ab4818c
mkrecny <mkrecny@gmail.com>
2011-10-28 15:24:45 -0400

    formatting in test

--- tests/simple.js
+++ tests/simple.js
@@ -16,6 +16,7 @@ var bspool = require('../index.js').build(opts);
 bspool.init(function(){
   var attempt = 1000; 
   var completed = 0;
+  
   for (var i = 0 ; i < 1000 ; i++){
     bspool.put({"job": i }, function(){
       completed += 1;
